03a062571b14347b0347a2a8aedd08df562770c7
rakib12212 <rakibulislamr83@gmail.com>
2025-03-25 03:26:45 +0600

    icon

--- src/app/dashboard/employee/page.jsx
+++ src/app/dashboard/employee/page.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const EmployeeHome = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-semibold mb-4">Employee Dashboard</h1>
       <p>Welcome to the Employee Dashboard Home Page.</p>
     </div>
--- src/app/dashboard/superAdmin/page.jsx
+++ src/app/dashboard/superAdmin/page.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const AdminHome = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-semibold mb-4">Super Admin Dashboard</h1>
       <p>Welcome to the Super Admin Dashboard Home Page.</p>
     </div>
